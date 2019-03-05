793b2141d09fa7813bf84d4cc1b9ab2ce819a1b2
Panakot3 <pahiiiiich@gmail.com>
2019-03-05 14:15:49 +0700

    Juun

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import './styles.scss';
 import './views/screen_01/index.scss';
 import './views/screen_02/index.scss';
 import './views/screen_03/index.scss';
+import './views/screen_04/index.scss';
+import './views/screen_05/index.scss';
