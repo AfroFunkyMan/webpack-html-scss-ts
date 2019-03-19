d2a4fbb6daff75c580892273ea6fa58621ce9fb8
Ruslan <afro.funky.lover@gmail.com>
2019-03-19 12:58:48 +0700

    revert to init

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,12 +6,8 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: "inline-source-map",
     devServer: {
-<<<<<<< HEAD
         port: 3000,
         host: '0.0.0.0',
-=======
->>>>>>> parent of 6f141ea... initial commit
-        contentBase: './dist',
     },
     plugins: [
         new webpack.NamedModulesPlugin(),
